a63106fb9331be2db185edae2b38cbc963d778e0
skxgogo <88844484+skxgogo@users.noreply.github.com>
2024-07-04 10:01:21 +0800

    Update utlis.py

--- utlis.py
+++ utlis.py
@@ -130,3 +130,30 @@ def get_I_and_U(preds, gt, thres=None):
     # intersection = np.logical_and(preds, gt).sum()
     # union =  np.logical_or(preds, gt).sum()
     return intersection, union
+    
+def march_and_save(occupancy, mcubes_thres, savename, smoothen=False):
+    '''
+        Convert volumetric occupancy cube to a 3D mesh
+        
+        Inputs:
+            occupancy: (H, W, T) occupancy volume with values going from 0 to 1
+            mcubes_thres: Threshold for marching cubes algorithm
+            savename: DAE file name to save
+            smoothen: If True, the mesh is binarized, smoothened, and then the
+                marching cubes is applied
+        Outputs:
+            None
+    '''
+    if smoothen:
+        occupancy = occupancy.copy()
+        occupancy[occupancy < mcubes_thres] = 0.0
+        occupancy[occupancy >= mcubes_thres] = 1.0
+        
+        occupancy = mcubes.smooth(occupancy, method='gaussian', sigma=1)
+        mcubes_thres = 0
+        
+    vertices, faces = mcubes.marching_cubes(occupancy, mcubes_thres)
+    
+    #vertices /= occupancy.shape[0]
+        
+    mcubes.export_mesh(vertices, faces, savename)
